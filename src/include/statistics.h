1d4086045a1a2fabe6bd5adfcef5c50a9889b747
robrodriguez99 <robrodriguez@itba.edu.ar>
2022-06-13 17:03:50 -0300

    basic statistics functions

---
+++ src/include/statistics.h
@@ -0,0 +1,31 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct socks5Stats{
+
+    uint64_t bytes_transfered;
+    uint64_t historic_connections;
+    uint16_t current_connections;
+    
+    bool spoofing;
+    bool authentication;
+    
+    int usr_amount;
+
+
+}socks5Stats;
+extern socks5Stats stats;
+
+void stats_init(void);
+void inc_current_connection(void);
+void dec_current_connection(void);
+void inc_historic_connections(void);
+
+void add_bytes_sent(uint64_t bytes);
+void inc_usr_amount(void);
+#endif
